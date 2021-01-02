ae931a63e09c18d8ee63f4df590a41f006895a52
Pratik Singh <67475692+ghostinlinux@users.noreply.github.com>
2021-01-02 19:31:06 +0530

    Add files via upload
    first commit

---
+++ jupyter-installer.sh
@@ -0,0 +1,27 @@
+#!/data/data/com.termux/files/usr/bin/bash
+apt update -y
+apt upgrade -y
+clear
+echo "Installing dependencies"
+sleep 1
+apt install python clang 
+clear
+pip install wheel
+clear
+pip install cython
+clear
+apt install libzmq -y
+clear
+pip install pyzmq
+clear
+echo "Installing Jupyter"
+pip install jupyter
+echo " "
+echo " "
+echo "----------------------"
+echo "Installation sucessful"
+echo "----------------------"
+echo "[_]Script by GHOSTINLINUX[_]"
+echo " "
+echo " "
+echo " Start JupyterNotebook by typing this command :- jupyter-notebook"
